d4f2d143ae8dfe2f2b2c40d6db1eb3c5519a123c
Tamal Saha <tamal@appscode.com>
2021-05-06 12:48:50 -0700

    Use k8s 1.21.0 toolchain
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/install/install.go
+++ apis/installer/install/install.go
@@ -17,7 +17,7 @@ limitations under the License.
 package install
 
 import (
-	"kmodules.xyz/installer/apis/installer/v1alpha1"
+	"go.bytebuilders.dev/installer/apis/installer/v1alpha1"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
