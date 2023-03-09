c97893c10f88133c25249e387b16470cd88600bd
Emruz Hossain <emruz@appscode.com>
2023-03-09 10:43:40 +0600

    Update chart + run make gen
    Signed-off-by: Emruz Hossain <emruz@appscode.com>

--- apis/installer/v1alpha1/zz_generated.deepcopy.go
+++ apis/installer/v1alpha1/zz_generated.deepcopy.go
@@ -23,7 +23,7 @@ package v1alpha1
 
 import (
 	installerv1alpha1 "go.openviz.dev/installer/apis/installer/v1alpha1"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	apiv1 "kmodules.xyz/objectstore-api/api/v1"
