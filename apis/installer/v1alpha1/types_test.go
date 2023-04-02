348de637f4f2f4c011ce198418f986c4a32731cf
Tamal Saha <tamal@appscode.com>
2023-04-02 14:07:39 -0700

    Add kube-auth-manager and kube-auth-proxy charts
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/types_test.go
+++ apis/installer/v1alpha1/types_test.go
@@ -38,6 +38,8 @@ func TestDefaultValues(t *testing.T) {
 		sc.TestCase{Obj: v1alpha1.DnsProxySpec{}},
 		sc.TestCase{Obj: v1alpha1.GrafanaSpec{}},
 		sc.TestCase{Obj: v1alpha1.KubedbUiSpec{}},
+		sc.TestCase{Obj: v1alpha1.KubeAuthManagerSpec{}},
+		sc.TestCase{Obj: v1alpha1.KubeAuthProxySpec{}},
 		sc.TestCase{Obj: v1alpha1.LicenseProxyserverSpec{}},
 		sc.TestCase{Obj: v1alpha1.MarketplaceUiSpec{}},
 		sc.TestCase{Obj: v1alpha1.MinioSpec{}},
