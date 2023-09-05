ac3601fd0f0cce740e82267d302dd1e03f51ed03
Tamal Saha <tamal@appscode.com>
2023-09-05 10:00:27 -0700

    Rework ui-presets charts
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/import-crds.sh
+++ hack/scripts/import-crds.sh
@@ -55,13 +55,10 @@ KUBEPACK_PRESET_TAG=${KUBEPACK_PRESET_TAG:-v0.0.2}
 HELM_X_APIMACHINERY_TAG=${HELM_X_APIMACHINERY_TAG:-master}
 
 crd-importer \
-    --input=https://github.com/x-helm/apimachinery/raw/${HELM_X_APIMACHINERY_TAG}/crds/charts.x-helm.dev_chartpresets.yaml \
     --input=https://github.com/x-helm/apimachinery/raw/${HELM_X_APIMACHINERY_TAG}/crds/charts.x-helm.dev_clusterchartpresets.yaml \
     --out=./charts/capi-ui-presets/crds
 
 crd-importer \
-    --input=https://github.com/kmodules/resource-metadata/raw/${KMODULES_RESOURCE_METADATA_TAG}/crds/ui.k8s.appscode.com_resourceeditors.yaml \
-    --input=https://github.com/x-helm/apimachinery/raw/${HELM_X_APIMACHINERY_TAG}/crds/charts.x-helm.dev_chartpresets.yaml \
     --input=https://github.com/x-helm/apimachinery/raw/${HELM_X_APIMACHINERY_TAG}/crds/charts.x-helm.dev_clusterchartpresets.yaml \
     --out=./charts/monitoring-ui-presets/crds
 
