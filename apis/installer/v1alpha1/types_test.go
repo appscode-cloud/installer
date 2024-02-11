783539a212ea06dad4a0361db633060f0bf28299
Tamal Saha <tamal@appscode.com>
2024-02-10 19:17:27 -0800

    Add inbox-ui chart (#306)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/types_test.go
+++ apis/installer/v1alpha1/types_test.go
@@ -40,6 +40,7 @@ func TestDefaultValues(t *testing.T) {
 		sc.TestCase{Obj: v1alpha1.DeployUiSpec{}},
 		sc.TestCase{Obj: v1alpha1.DnsProxySpec{}},
 		sc.TestCase{Obj: v1alpha1.GrafanaSpec{}},
+		sc.TestCase{Obj: v1alpha1.InboxUiSpec{}},
 		sc.TestCase{Obj: v1alpha1.KubedbUiSpec{}},
 		sc.TestCase{Obj: v1alpha1.LicenseProxyserverSpec{}},
 		sc.TestCase{Obj: v1alpha1.MarketplaceUiSpec{}},
