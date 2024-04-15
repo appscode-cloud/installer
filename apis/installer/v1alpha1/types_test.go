bf710175cbe8d676181a307d6dc9479577ced8c9
Tamal Saha <tamal@appscode.com>
2024-04-15 04:49:38 -0700

    Fix reloader security context (#359)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/types_test.go
+++ apis/installer/v1alpha1/types_test.go
@@ -57,7 +57,7 @@ func TestDefaultValues(t *testing.T) {
 		sc.TestCase{Obj: v1alpha1.SmtprelaySpec{}},
 		sc.TestCase{Obj: v1alpha1.StashPresetsSpec{}},
 		sc.TestCase{Obj: v1alpha1.NatsSpec{}, File: "https://github.com/nats-io/k8s/raw/nats-0.19.17/helm/charts/nats/values.yaml"},
-		sc.TestCase{Obj: v1alpha1.ReloaderSpec{}, File: "https://github.com/stakater/Reloader/raw/v1.0.76/deployments/kubernetes/chart/reloader/values.yaml"},
+		sc.TestCase{Obj: v1alpha1.ReloaderSpec{}, File: "https://github.com/stakater/Reloader/raw/v1.0.79/deployments/kubernetes/chart/reloader/values.yaml"},
 	)
 	checker.TestAll(t)
 }
