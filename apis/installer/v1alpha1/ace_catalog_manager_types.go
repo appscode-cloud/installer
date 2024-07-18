2f55ade6e2d7b0dc956e103c58a68e41a1f1d04c
Tamal Saha <tamal@appscode.com>
2024-07-18 13:31:25 -0400

    Pass private regsitry config catalog-manager
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/ace_catalog_manager_types.go
+++ apis/installer/v1alpha1/ace_catalog_manager_types.go
@@ -19,6 +19,7 @@ package v1alpha1
 import (
 	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"kmodules.xyz/resource-metadata/apis/shared"
 )
 
 const (
@@ -44,6 +45,8 @@ type CatalogManager struct {
 
 // CatalogManagerSpec is the schema for Identity Server values file
 type CatalogManagerSpec struct {
+	//+optional
+	Proxies shared.RegistryProxies `json:"proxies"`
 	//+optional
 	NameOverride string `json:"nameOverride"`
 	//+optional
