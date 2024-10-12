e39f1370f6bc2be65512ca150c1798638c63918d
Tamal Saha <tamal@appscode.com>
2024-10-12 05:01:11 -0700

    Update cluster-profile to v0.0.7
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- catalog/sync-gcp-mp-images.sh
+++ catalog/sync-gcp-mp-images.sh
@@ -93,7 +93,7 @@ crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/addon-manage
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-auth:v0.0.5 $IMAGE_REGISTRY/cluster-auth:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-gateway-manager:v1.9.1 $IMAGE_REGISTRY/cluster-gateway-manager:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-gateway:v1.9.1 $IMAGE_REGISTRY/cluster-gateway:$TAG
-crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-profile:v0.0.6 $IMAGE_REGISTRY/cluster-profile:$TAG
+crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-profile:v0.0.7 $IMAGE_REGISTRY/cluster-profile:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-proxy:v0.5.0 $IMAGE_REGISTRY/cluster-proxy:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/clusteradm:v0.9.0 $IMAGE_REGISTRY/clusteradm:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/fluxcd-addon:v0.0.4 $IMAGE_REGISTRY/fluxcd-addon:$TAG
