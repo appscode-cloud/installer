9a89bc8ff25f8e91658d6a33bf5828fe4eb27029
Imtiaz Uddin <imtiaz@appscode.com>
2024-11-15 18:08:53 +0600

    Add ApiSecret field in AceDeploymentContext
    Signed-off-by: Imtiaz Uddin <imtiaz@appscode.com>

--- apis/installer/v1alpha1/ace_options_types.go
+++ apis/installer/v1alpha1/ace_options_types.go
@@ -385,6 +385,7 @@ type AceDeploymentContext struct {
 	InstallerName  string         `json:"installerName"`
 	UploadID       string         `json:"uploadID"`
 	Version        string         `json:"version"`
+	ApiSecret      string         `json:"apiSecret"`
 	// +optional
 	RequestedDomain      string `json:"requestedDomain"`
 	HostedDomain         string `json:"hostedDomain,omitempty"`
