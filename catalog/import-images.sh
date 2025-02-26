9d5760610377b4ebedc28db2b950b215b3ffcb73
Tamal Saha <tamal@appscode.com>
2025-02-26 00:12:10 -0800

    Update catalog manager
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- catalog/import-images.sh
+++ catalog/import-images.sh
@@ -283,7 +283,7 @@ $CMD push --allow-nondistributable-artifacts --insecure images/appscode-aceshift
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-b3-v2025.2.28.tar $IMAGE_REGISTRY/appscode/b3:v2025.2.28
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-capa-vpc-peering-operator-v0.0.4.tar $IMAGE_REGISTRY/appscode/capa-vpc-peering-operator:v0.0.4
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-capi-ops-manager-v0.0.2.tar $IMAGE_REGISTRY/appscode/capi-ops-manager:v0.0.2
-$CMD push --allow-nondistributable-artifacts --insecure images/appscode-catalog-manager-v0.2.2.tar $IMAGE_REGISTRY/appscode/catalog-manager:v0.2.2
+$CMD push --allow-nondistributable-artifacts --insecure images/appscode-catalog-manager-v0.2.3.tar $IMAGE_REGISTRY/appscode/catalog-manager:v0.2.3
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-cluster-presets-v0.0.8.tar $IMAGE_REGISTRY/appscode/cluster-presets:v0.0.8
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-cluster-ui-0.9.14.tar $IMAGE_REGISTRY/appscode/cluster-ui:0.9.14
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-config-syncer-v0.15.2-ent.tar $IMAGE_REGISTRY/appscode/config-syncer:v0.15.2-ent
