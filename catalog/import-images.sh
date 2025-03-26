bce03c94ce7dddc0e585f3627d1fc4613ef99de1
Tamal Saha <tamal@appscode.com>
2025-03-26 01:30:39 -0700

    update
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- catalog/import-images.sh
+++ catalog/import-images.sh
@@ -376,7 +376,7 @@ $CMD push --allow-nondistributable-artifacts --insecure images/loft-sh-vcluster-
 $CMD push --allow-nondistributable-artifacts --insecure images/voyagermesh-echoserver-v20221109.tar $IMAGE_REGISTRY/voyagermesh/echoserver:v20221109
 $CMD push --allow-nondistributable-artifacts --insecure images/voyagermesh-envoy-v1.33.0-ac.tar $IMAGE_REGISTRY/voyagermesh/envoy:v1.33.0-ac
 $CMD push --allow-nondistributable-artifacts --insecure images/voyagermesh-gateway-converter-v0.0.1.tar $IMAGE_REGISTRY/voyagermesh/gateway-converter:v0.0.1
-$CMD push --allow-nondistributable-artifacts --insecure images/voyagermesh-gateway-v1.3.0.tar $IMAGE_REGISTRY/voyagermesh/gateway:v1.3.0
+$CMD push --allow-nondistributable-artifacts --insecure images/voyagermesh-gateway-v1.3.1.tar $IMAGE_REGISTRY/voyagermesh/gateway:v1.3.1
 $CMD push --allow-nondistributable-artifacts --insecure images/hashicorp-vault-csi-provider-1.4.1.tar $IMAGE_REGISTRY/hashicorp/vault-csi-provider:1.4.1
 $CMD push --allow-nondistributable-artifacts --insecure images/hashicorp-vault-secrets-operator-0.4.3.tar $IMAGE_REGISTRY/hashicorp/vault-secrets-operator:0.4.3
 $CMD push --allow-nondistributable-artifacts --insecure images/hashicorp-vault-1.15.2.tar $IMAGE_REGISTRY/hashicorp/vault:1.15.2
