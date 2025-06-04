356ec25a440456bb8abdc5004e85d7211e25f7fe
Imtiaz Uddin <imtiaz@appscode.com>
2025-06-05 02:35:00 +0600

    Separate platform configs & set inbox-server emailDomain field in config
    Signed-off-by: Imtiaz Uddin <imtiaz@appscode.com>

--- apis/installer/v1alpha1/ace_platform_api.go
+++ apis/installer/v1alpha1/ace_platform_api.go
@@ -109,6 +109,8 @@ type AceAPISettings struct {
 
 type AceAPISettingsSecretName struct {
 	AceSettingsSecretName         `json:",inline,omitempty"`
+	PlatformSystemConfig          string `json:"platformSystemConfig"`
+	PlatformUserConfig            string `json:"platformUserConfig"`
 	GcpMarketplaceReportingSecret string `json:"gcpMarketplaceReportingSecret"`
 }
 
