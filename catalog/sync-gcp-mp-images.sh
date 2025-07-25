0abaeea0bf7b6d25b3d022078dd7f46905882121
Tamal Saha <tamal@appscode.com>
2025-07-25 16:27:15 -0700

    Use fluxcd 2.16 (#939)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- catalog/sync-gcp-mp-images.sh
+++ catalog/sync-gcp-mp-images.sh
@@ -97,7 +97,7 @@ crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-gate
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-profile:v0.10.0 $IMAGE_REGISTRY/cluster-profile:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/cluster-proxy:v0.7.0 $IMAGE_REGISTRY/cluster-proxy:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/clusteradm:v1.0.0 $IMAGE_REGISTRY/clusteradm:$TAG
-crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/fluxcd-addon:v0.0.8 $IMAGE_REGISTRY/fluxcd-addon:$TAG
+crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/fluxcd-addon:v0.0.9 $IMAGE_REGISTRY/fluxcd-addon:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/managed-serviceaccount:v0.8.1 $IMAGE_REGISTRY/managed-serviceaccount:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/placement:v0.15.2 $IMAGE_REGISTRY/placement:$TAG
 crane cp --allow-nondistributable-artifacts ghcr.io/kluster-manager/registration-operator:v0.15.2 $IMAGE_REGISTRY/registration-operator:$TAG
