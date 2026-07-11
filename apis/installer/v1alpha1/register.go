2b0be2b0a4eb18d666c1aeb3342ca9e9793f1bb4
Tamal Saha <tamal@appscode.com>
2026-07-11 17:54:39 +0600

    Modernize golangci-lint config
    - Enable bodyclose and prealloc linters
    - Move exclude-files/exclude-dirs to linters.exclusions.paths (golangci-lint
      v2 location) and fix over-escaped regex generated.*\\.go -> generated.*\.go
    - Switch formatter from gofmt to gofumpt and drop the interface{} -> any
      rewrite rule (gofumpt is a stricter superset)
    - Apply resulting gofumpt formatting
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/register.go
+++ apis/installer/v1alpha1/register.go
@@ -53,7 +53,8 @@ func Resource(resource string) schema.GroupResource {
 
 // Adds the list of known types to api.Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemeGroupVersion,
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
 		&Acaas{},
 		&AcaasList{},
 		&AccountsUi{},
@@ -150,7 +151,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&WebsiteList{},
 	)
 
-	scheme.AddKnownTypes(SchemeGroupVersion,
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
 		&metav1.Status{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
